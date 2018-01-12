f04e9affc735d760017fa7f07c345f9beb9674ae
kagami <kagamiwomiru@me.com>
2018-01-12 12:26:25 +0900

    水増し画像をかくにんできるようにしました

---
+++ draw_img.py
@@ -0,0 +1,32 @@
+import glob
+import os
+import shutil
+import numpy as np
+from scipy.misc import toimage
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+def draw_img(datagen):
+#出力先ディレクトリを指定
+    temp_dir="./result/tmpimg"
+    result_images="./result/out.jpg"
+    os.mkdir(temp_dir)
+
+#generatorから9個の画像を生成
+#xはサンプル数で考慮(1枚ならbatch_size=1)
+    for i in range(9):
+        batch=datagen.next()
+
+    #生成した画像を3x3で描画
+    images = glob.glob(os.path.join(temp_dir,"*.jpg"))
+    fig=plt.figure()
+    gs=gridspec.GridSpec(3,3)
+    gs.update(wspace=0.1,hspace=0.1)
+    for i in range(9):
+        img=load_img("test/Kagami/out_010.jpg")
+        plt.subplot(gs[i])
+        plt.imshow(img,aspect='auto')
+        plt.axis("off")
+    plt.savefig(result_images)
+    shutil.rmtree(temp_dir)
+
--- face_keras.py
+++ face_keras.py
@@ -24,7 +24,8 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 img_size = 224
 batch_size = 32
 #以下ディレクトリに入っている画像を読み込む
-root_dir = "./face/"
+root_dir = "./test/"
+#root_dir = "./face/"
 #学習データを何周するか
 epochs=20
 #ログファイル
@@ -54,7 +55,9 @@ def main():
 def data_augmentation():
     #学習画像データを水増し（データ拡張）を行う
     mizumashi_data=ImageDataGenerator()
-    mizumashi_generator=mizumashi_data.flow_from_directory(directory=root_dir,target_size=(img_size,img_size),batch_size=batch_size,shuffle=True)
+    miz_gen=mizumashi_generator=mizumashi_data.flow_from_directory(directory=root_dir,target_size=(img_size,img_size),batch_size=batch_size,shuffle=True)
+    import draw_img as di
+    di.draw_img(miz_gen)
     #テスト画像データを水増しする。
     val_datagen=ImageDataGenerator()
     val_gen=val_generator=val_datagen.flow_from_directory(directory=root_dir,target_size=(img_size,img_size),batch_size=batch_size,shuffle=False)
