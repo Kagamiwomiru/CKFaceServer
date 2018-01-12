ea97757c1c1c1ec0fd3593da7c9bc94520d1c16f
Kagamiwomiru_MBP <kagamiwomiru@me.com>
2018-01-12 14:29:44 +0900

    決してはいけないファイルを復活させました

--- exe_client.sh
+++ exe_client.sh
@@ -1,4 +1,4 @@
 scp face/face-model.h5 Raspi:CKFace/face-model.h5
 scp face/face.json Raspi:CKFace/face.json
 scp face/categories.json Raspi:CKFace/categories.json
-ssh Raspi 'cd CKFace/ ;python3 keras_auth.py'
+sshpass -p issi0505 ssh Raspi 'cd CKFace/ ;python3 keras_auth.py'
