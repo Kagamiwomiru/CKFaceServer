f15c1d0f8391c1ebf262e46175556d9ce2fcdfa3
Kagamiwomiru <kagamiwomiru@gmail.com>
2018-01-23 21:06:42 +0900

    教師あり学習

--- face_keras.py
+++ face_keras.py
@@ -1,4 +1,9 @@
-# https://github.com/fchollet/keras/blob/master/examples/cifar10_cnn.py
+
+# coding: utf-8
+
+# In[12]:
+
+
 import os
 import numpy as np
 import keras
@@ -7,46 +12,38 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
 from keras.backend import tensorflow_backend as backend
+from keras.preprocessing.image import ImageDataGenerator
+import json
+
 root_dir = "./face/"
-image_size = 96
+batch_size=32
 epochs=10
-
-def main():
-    X_train, X_test, y_train, y_test = np.load("./face/face.npy")
-    X_train = X_train.astype("float") / 256
-    X_test  = X_test.astype("float")  / 256
-    y_train = np_utils.to_categorical(y_train, nb_classes)
-    y_test  = np_utils.to_categorical(y_test, nb_classes)
-    
-		
-    model = model_train(X_train, y_train)
-    model_eval(model, X_test, y_test)
+img_size = 224
+classFile="./face/categories.json"
 
 
-def load_people():
-    #categories = ["Kagami","Kato","Uchiyama","the_others"]
-    categories=[]
-    for x in os.listdir(root_dir):
-     if os.path.isdir(root_dir + x):
-         categories.append(x)
-    print(categories)
-    #リスト書き出し
-    f = open("./categories.txt","w")
-    for x in categories:
-       f.write(str(x) + "\n")
-    f.close()
-
-    #people=3
-    return len(categories)
+# In[2]:
 
 
+def data_augmentation():
+    #学習画像データを水増し（データ拡張）を行う
+    mizumashi_data=ImageDataGenerator()
+    mizumashi_generator=mizumashi_data.flow_from_directory(directory=root_dir,target_size=(img_size,img_size),batch_size=batch_size,shuffle=True)
+    #テスト画像データを水増しする。
+    val_datagen=ImageDataGenerator()
+    val_gen=val_generator=val_datagen.flow_from_directory(directory=root_dir,target_size=(img_size,img_size),batch_size=batch_size,shuffle=False)
+    valX,valy=val_gen.next()
+    return (mizumashi_generator,val_generator,valX,valy)
 
 
-def build_model(in_shape):
+# In[3]:
+
+
+def build_model():
     model = Sequential()
     model.add(Convolution2D(32, 3, 3,
 	border_mode='same',
-	input_shape=in_shape))
+	input_shape=(img_size,img_size,3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
@@ -59,30 +56,75 @@ def build_model(in_shape):
     model.add(Dense(512))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(nb_classes))
+    model.add(Dense(12))
     model.add(Activation('softmax'))
-    model.compile(loss='binary_crossentropy',
-	optimizer='rmsprop',
-	metrics=['accuracy'])
+    model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
-def model_train(X, y):
-    model = build_model(X.shape[1:])
-    tf_lg=keras.callbacks.TensorBoard(log_dir="./logs", histogram_freq=1)
-    log=[tf_lg]
-    history = model.fit(X, y, batch_size=32, nb_epoch=epochs,callbacks=log,validation_split=0.1)
+
+# In[4]:
+
+
+def learning(model,mizumashi_generator,val_generator):
+    tf_lg=keras.callbacks.TensorBoard(log_dir="./logs",write_grads=True)
+    es_cb=keras.callbacks.EarlyStopping(monitor='val_loss',patience=0,verbose=1,mode='auto')
+
+    log=[tf_lg,es_cb]
+    history = model.fit_generator(mizumashi_generator,
+                                  validation_data=val_generator,
+                                  steps_per_epoch=mizumashi_generator.samples// batch_size,
+                                  validation_steps=val_generator.samples // batch_size,
+                                  epochs=epochs,callbacks=log,
+                                  verbose=1)
     json_string=model.to_json()
     open('./face/face.json','w').write(json_string)
     hdf5_file = "./face/face-model.h5"
     model.save_weights(hdf5_file)
     return model
 
+
+
+# In[8]:
+
+
 def model_eval(model, X, y):
-    score = model.evaluate(X, y)
+    score = model.evaluate(X, y,batch_size=batch_size)
     print('loss=', score[0])
     print('accuracy=', score[1])
-    backend.clear_session()
-if __name__ == "__main__":
-		nb_classes=load_people()
-		main()
+
+
+
+# In[5]:
+
+
+mizumashi_generator,val_generator,valX,valy=data_augmentation()
+
+
+# In[6]:
+
+
+model=build_model()
+
+
+# In[7]:
+
+
+learning(model,mizumashi_generator,val_generator)
+
+
+# In[13]:
+
+
+# class と indexの対応を逆にする
+indices_to_class = dict((v, k)   for k, v in mizumashi_generator.class_indices.items())
+print(indices_to_class)
+f=open(classFile,'w')
+json.dump(indices_to_class,f)
+
+
+# In[14]:
+
+
+model_eval(model, valX, valy)
+backend.clear_session()
 
